5f9148326e46fa5bb0f2bb73014166713b592d33
SoyDiego <diegofranchina@gmail.com>
2020-05-10 06:03:48 +0200

    Upload website with responsive 90% done

---
+++ js/slider.js
@@ -0,0 +1,119 @@
+var mySwiper = new Swiper(".swiper-container", {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 1,
+	centeredSlides: true,
+	autoplay: {
+		delay: 2000,
+	},
+
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		576: {
+			slidesPerView: 2,
+			spaceBetween: 10,
+		},
+		768: {
+			slidesPerView: 3,
+			spaceBetween: 100,
+		},
+		1024: {
+			slidesPerView: 4,
+			spaceBetween: 100,
+		},
+	},
+
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: ".swiper-scrollbar",
+	// },
+});
+
+var mySwiper2 = new Swiper(".swiper-2", {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 1,
+	centeredSlides: true,
+	autoplay: {
+		delay: 2000,
+	},
+
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		576: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 10,
+		},
+		1024: {
+			slidesPerView: 4,
+			spaceBetween: 50,
+		},
+	},
+
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: ".swiper-scrollbar",
+	// },
+});
+
+var mySwiper3 = new Swiper(".swiper-3", {
+	// Optional parameters
+	loop: true,
+	slidesPerView: 1,
+	centeredSlides: true,
+	autoplay: {
+		delay: 2000,
+	},
+
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		576: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		768: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 10,
+		},
+	},
+
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: ".swiper-scrollbar",
+	// },
+});
